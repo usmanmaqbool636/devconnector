d926cbc61bc3de9aed3c0efc2bc689eae89e8ce2
Usman Maqbool <usmanmaqbool636@gmail.com>
2018-10-01 10:37:37 +0500

    initial commit 1

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+
+module.exports={
+    MongoUrl:
+    'mongodb://usman:usman123@ds115543.mlab.com:15543/merndb'
+}
---
+++ server.js
@@ -0,0 +1,21 @@
+const express =require('express');
+const app = express();
+const mongoose =require('mongoose');
+const db = require('./config/keys').MongoUrl;
+const posts=require('./routes/api/posts');
+const profile=require('./routes/api/profile');
+const user=require('./routes/api/user');
+mongoose.connect(db)
+    .then(()=>{
+        console.log('mongo DataBase connect');
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+
+app.get('/',(req,res)=>res.send('hello world'));
+app.use('/api/posts',posts);
+app.use('/api/profile',profile);
+app.use('/api/user',user);
+const port = process.env.port || 5000;
+app.listen(port,()=>console.log(`server is runnig on port number ${port}`));
